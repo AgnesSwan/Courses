02544e62dbda0ad62dbd1ffdbcb2ab6716e03016
AgnesSwan <agnes.swan7@gmail.com>
2021-05-01 14:51:09 +0200

    Dodaj pliki projektów.

---
+++ CompetenciesDevelopment/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CompetenciesDevelopment.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult getCourses()
+        {
+            return Ok("jest ok");
+            // I oznacza interfejs/ wynikiem może byc wszystko
+        }
+    }
+}
---
+++ CompetenciesDevelopment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompetenciesDevelopment.WebApiApplication" Language="C#" %>
