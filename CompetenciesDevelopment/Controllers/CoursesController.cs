cf72a63c0b8b2018bd0d4a4eca0ecb8400071dcc
AgnesSwan <agnes.swan7@gmail.com>
2021-05-01 18:06:13 +0200

    added db connection, get list , find id

--- CompetenciesDevelopment/Controllers/CoursesController.cs
+++ CompetenciesDevelopment/Controllers/CoursesController.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using CompetenciesDevelopment.Models;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace CompetenciesDevelopment.Controllers
 {
     public class CoursesController : ApiController
     {
+        KursyEntities db = new KursyEntities();
+
         [HttpGet]
         public IHttpActionResult getCourses()
         {
-            return Ok("jest ok");
+            return Ok(db.Courses.ToList());
             // I oznacza interfejs/ wynikiem może byc wszystko
+        }
+
+        [HttpGet]
+        public IHttpActionResult getDetails(int id)
+        {
+            return Ok(db.Courses.Find(id));
         }
     }
 }
